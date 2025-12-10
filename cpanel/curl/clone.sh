b5813a40e5f3fc26db795801841e2930f9725928
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-10 19:52:49 +0530

    Update and rename clone_via_curl.md to clone.sh

--- cpanel/curl/clone.sh
+++ cpanel/curl/clone.sh
@@ -1,5 +1,1 @@
-## Clone installation via cURL
-
-```php
 curl -d "softsubmit=1" -d "softdomain=example.com" -d "softdirectory=wp" -d "softdb=wpdb" "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=sclone&insid=26_12345&api=json"
-```
