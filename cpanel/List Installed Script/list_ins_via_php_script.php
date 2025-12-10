295b826600574b08b3319bbf2df52f2e20abe02c
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-10 11:57:31 +0530

    Update list_ins_via_php_script.php

--- cpanel/List Installed Script/list_ins_via_php_script.php
+++ cpanel/List Installed Script/list_ins_via_php_script.php
@@ -1,1 +1,42 @@
-curl "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=installations&api=json"
+<?php
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+       '&api=serialize'.
+       '&act=installations';
+
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+ curl_setopt($ch, CURLOPT_POST, 1);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+if(!empty($res['error'])){
+
+     // Error
+     echo 'Some error occurred';
+     print_r($res['error']);
+
+}else{
+     
+     print_r($res['installations']);
+
+}
+
+?>
