1aab33bc4605773ed0caa7005e767679d36661a8
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 16:37:20 +0530

    Create admin_edit_plan.sh

---
+++ cpanel/curl/admin_edit_plan.sh
@@ -0,0 +1,1 @@
+curl -d "saveplan=1" -d "planname=1" -d "cpplan_CPPlanName" -d "resellers_abc=1" -d "users_xyz=1" -d "scripts_543=1" -d "scripts_72=1" "https://user:password@domain.com:2087/url/to/softaculous/index.php?act=editplans&plan=plan1&api=json"
