5de7e1e84174488bc1b146eb81370ea1542a3a18
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 17:48:20 +0530

    Create restore.php

---
+++ cpanel/php/restore.php
@@ -0,0 +1,33 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=restore'.
+			'&restore=backup_time_insid.tar.gz';
+
+$post = array('restore_ins' => '1',
+              'restore_dir' => '1', // Pass this if you want to restore the directory
+              'restore_datadir' => '1', // Pass this if you want to restore the data directory
+              'restore_db' => '1', // Pass this if you want to restore the database
+		);
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+
+?>
