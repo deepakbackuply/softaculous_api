9577cc89eb886df952325e851d5342777bf93626
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:05:30 +0530

    Update create_template.php

--- cpanel/php/create_template.php
+++ cpanel/php/create_template.php
@@ -16,7 +16,7 @@ $post = array('template_name' => 'My Template',
 // Set the curl parameters.
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 if(!empty($post)){
