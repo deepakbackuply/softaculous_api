2784c74e9bbd6c3252961ab6b392da04fe6cd1f6
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:08:12 +0530

    Create restore_template.php

---
+++ cpanel/php/restore_template.php
@@ -0,0 +1,55 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=restore_template'.
+			'&restore=my-template.tar.gz'.
+			'&autoid=abcdefghijklmnopqrstuvwxyz0123456789';
+
+$post = array('softproto' => 1,
+              'softsubmit' => 1,
+              'softdomain' => 'domain.com',
+              'softdb' => 'wp123',
+              'site_name' => 'myblog',
+              'softdirectory' => 'restoredsite',
+	      );
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+if(!empty($post)){
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done_msg'])){
+
+	echo $res['done_msg'];
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+// Print the entire output just incase !
+print_r($res);
+
+?>
