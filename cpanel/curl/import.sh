ed90ed009f083235340db896e85a68cd74d47ab9
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 15:04:14 +0530

    Create import.sh

---
+++ cpanel/curl/import.sh
@@ -0,0 +1,1 @@
+curl -d "softsubmit=1" -d "softdomain=example.com" -d "softdirectory=wp" "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=import&soft=26&api=json"
