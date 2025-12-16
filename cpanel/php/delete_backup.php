28d05c17ce116a65679f91aa9af5735250626721
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 17:51:08 +0530

    Create delete_backup.php

---
+++ cpanel/php/delete_backup.php
@@ -0,0 +1,28 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=backups'.
+			'&remove=backup_time_insid.tar.gz';
+
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+
+?>
