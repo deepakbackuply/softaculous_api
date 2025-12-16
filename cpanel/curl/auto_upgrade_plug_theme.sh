272b2795f9fcf74117b3e7f0ccd157fe06f5b400
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 16:04:54 +0530

    Create auto_upgrade_plug_theme.sh

---
+++ cpanel/curl/auto_upgrade_plug_theme.sh
@@ -0,0 +1,1 @@
+curl -d "insid=26_31793" -d "auto_upgrade_plugins=1" -d "auto_upgrade_themes=1" "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=wordpress&api=json"
