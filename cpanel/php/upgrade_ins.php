547f9845d5b443f4bb72a72dfdcda17e00beb68f
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 17:10:11 +0530

    Create upgrade_ins.php

---
+++ cpanel/php/upgrade_ins.php
@@ -0,0 +1,58 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=upgrade'.
+                        '&insid=26_12345';
+
+$post = array('softsubmit' => '1');
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done'])){
+
+	// There might be some task that the user has to perform
+	if(!empty($res['setupcontinue'])){
+
+		echo 'Please visit the following URL to complete upgrade : '.$res['setupcontinue'];	
+
+	// It upgraded
+	}else{
+
+		echo 'Upgraded successfully. URL to Installation : '.$res['__settings']['softurl'];	
+
+	}
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+// Print the entire output just incase !
+print_r($res);
+
+?>
