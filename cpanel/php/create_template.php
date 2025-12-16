e1d61fdf794120b4d24ae60e12ff1b3e4f4a35de
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:03:33 +0530

    Create create_template.php

---
+++ cpanel/php/create_template.php
@@ -0,0 +1,53 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=create_template'.
+			'&insid=26_12345'.
+			'&autoid=abcdefghijklmnopqrstuvwxyz0123456789';
+
+$post = array('template_name' => 'My Template',
+              'template_type' => 1,
+              'createtemplate' => 1,
+              'disallow_plugins' => 'on',
+	      );
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+if(!empty($post)){
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done'])){
+
+	echo $res['done_msg'];
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+// Print the entire output just incase !
+print_r($res);
+
+?>
