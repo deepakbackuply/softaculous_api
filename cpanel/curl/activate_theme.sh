3ee01c6ce026268402a64ba6fa835b528e1d3260
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 16:20:45 +0530

    Create activate_theme.sh

---
+++ cpanel/curl/activate_theme.sh
@@ -0,0 +1,1 @@
+curl -d "insid=26_31793" -d "type=themes" -d "slug=twentytwenty/style.css" -d "activate=1" "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=wordpress&api=json"
