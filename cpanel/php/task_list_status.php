d84da946af5d180271a258a2789d7d8c048bd88d
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:09:35 +0530

    Create task_list_status.php

---
+++ cpanel/php/task_list_status.php
@@ -0,0 +1,23 @@
+<?php 
+
+// The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+			'&act=eu_tasklist'.
+			'&ssk=abcdefghijklmnopqrstuvwxyz0123456789';
+
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+
+?>
