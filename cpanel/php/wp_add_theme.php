dfc4c2f85a8d23b98599645fe9fe6d372c68a3fc
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:26:47 +0530

    Create wp_add_theme.php

---
+++ cpanel/php/wp_add_theme.php
@@ -0,0 +1,49 @@
+<?php 
+
+//The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+                        '&sets_name=SET-NAME'.
+                        '&themes=1'.
+                        '&add_plugins_themes_data=1'.
+			'&act=manage_sets';
+
+$post = array('add_plugins_themes_data_slugs' => array('popularfx'), //Slug name
+              'add_plugins_themes_data_names' => array('PopularFx') //Theme name
+);
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done'])){
+
+	print_r($res);
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+?>
