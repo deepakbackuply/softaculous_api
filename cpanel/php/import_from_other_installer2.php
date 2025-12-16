2398218dc58faf95c8e0a0c5c3e375dc1739283f
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 17:28:03 +0530

    Create import_from_other_installer2.php

---
+++ cpanel/php/import_from_other_installer2.php
@@ -0,0 +1,26 @@
+<?php 
+
+//The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+       '&api=serialize'.
+       '&act=sync';
+$post = array( 'softsubmit' => 1, 
+               'approved' =>     array('cbpny97zd5kcsk4coo8gws084','dxw755lmeb4s4cw40o8o8kk44')); 
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+if(!empty($post)){
+ curl_setopt($ch, CURLOPT_POST, 1);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+// Get response from the server.
+$resp = curl_exec($ch);
+$res = unserialize($resp);
+print_r($res);  
+
+?>
