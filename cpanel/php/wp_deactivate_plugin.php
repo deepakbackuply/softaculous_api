73008a7abe9d88ddc0177a572e9cf284e3818134
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 19:10:04 +0530

    Create wp_deactivate_plugin.php

---
+++ cpanel/php/wp_deactivate_plugin.php
@@ -0,0 +1,48 @@
+<?php 
+
+//The URL
+$url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
+			'&api=serialize'.
+                        '&act=wordpress';
+
+$post = array('insid' => '26_31793',
+              'type' => 'plugins',
+              'slug' => 'akismet/akismet.php',
+              'deactivate' => '1'
+);
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done'])){
+
+	print_r($res);
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+?>
