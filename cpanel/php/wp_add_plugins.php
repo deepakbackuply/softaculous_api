43905b962be3192239cbf5e90ae7fa17d98569a0
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-16 18:25:53 +0530

    Update wp_add_plugins.php

--- cpanel/php/wp_add_plugins.php
+++ cpanel/php/wp_add_plugins.php
@@ -15,7 +15,7 @@ $post = array('add_plugins_themes_data_slugs' => array('loginizer'), //Slug name
 // Set the curl parameters.
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // Turn off the server and peer verification (TrustManager Concept).
