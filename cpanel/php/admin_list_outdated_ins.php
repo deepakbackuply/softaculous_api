b9aafde209a090d97b8b2c700c4c871a57c44f5a
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 11:51:28 +0530

    Create admin_list_outdated_ins.php

---
+++ cpanel/php/admin_list_outdated_ins.php
@@ -0,0 +1,48 @@
+<?php 
+
+// URL
+$url = 'http://admin.controlpanel.com:PORT/url/to/softaculous/index.php?'.
+			'&api=serialize'.
+			'&act=installations'.
+			'&show=outdated';
+
+$post = array('listinstallations' => 1,
+              //'users' => 'root;user1;', // Pass this if you want the installation list of specific users
+              //'scripts' => 'WordPress;Joomla;', // Pass this if you want the installation list of specific scripts
+              );
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res)){
+
+	print_r($res);
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+?>
