5b8f93d1629e6fa22deb572d7d409022c595a7c0
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 11:55:09 +0530

    Create admin_update_real_ver.php

---
+++ cpanel/php/admin_update_real_ver.php
@@ -0,0 +1,38 @@
+<?php 
+
+// URL
+$url = 'http://admin.controlpanel.com:PORT/url/to/softaculous/index.php?'.
+			'&api=serialize'.
+			'&act=installations';
+
+$post = array('listinstallations' => 1, 'only_realversion' => 1, 'list' => array('username-26_68351', 'username-26_68352'));
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res)){
+
+	print_r($res);
+
+}
+
+?>
