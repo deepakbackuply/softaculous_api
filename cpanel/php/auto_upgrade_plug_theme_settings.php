8dd65d5b707165fab4a22e1bd560f1228d2ceead
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 13:52:01 +0530

    Update auto_upgrade_plug_theme_settings.php

--- cpanel/php/auto_upgrade_plug_theme_settings.php
+++ cpanel/php/auto_upgrade_plug_theme_settings.php
@@ -2,14 +2,14 @@
 
 //The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-                        '&act=wordpress';
+			  '&api=serialize'.
+              '&act=wordpress';
 
 $post = array('insid' => '26_31793',
               'auto_upgrade_plugins' => '1',
               'auto_upgrade_themes' => '1',
               'save' => '1'
-);
+			);
 
 // Set the curl parameters.
 $ch = curl_init();
