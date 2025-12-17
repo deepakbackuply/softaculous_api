28f36b003e760cb90990f084e0c0de41a649063d
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 11:46:25 +0530

    Update wp_activate_plugin.php

--- cpanel/php/wp_activate_plugin.php
+++ cpanel/php/wp_activate_plugin.php
@@ -14,7 +14,7 @@ $post = array('insid' => '26_31793',
 // Set the curl parameters.
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $time);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // Turn off the server and peer verification (TrustManager Concept).
