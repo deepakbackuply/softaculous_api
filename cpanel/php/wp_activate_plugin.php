2da0a139b693f8d8972dd2ee7e6a4e48ec5ddb6d
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 15:04:43 +0530

    Update wp_activate_plugin.php

--- cpanel/php/wp_activate_plugin.php
+++ cpanel/php/wp_activate_plugin.php
@@ -2,14 +2,14 @@
 
 //The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-                        '&act=wordpress';
+			  '&api=serialize'.
+              '&act=wordpress';
 
 $post = array('insid' => '26_31793',
               'type' => 'plugins',
               'slug' => 'akismet/akismet.php',
               'activate' => '1'
-);
+			);
 
 // Set the curl parameters.
 $ch = curl_init();
