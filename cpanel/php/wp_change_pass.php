d00f5023a6e7768a35c8fd84df4b144b0042ffdb
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 15:07:17 +0530

    Update wp_change_pass.php

--- cpanel/php/wp_change_pass.php
+++ cpanel/php/wp_change_pass.php
@@ -2,14 +2,14 @@
 
 //The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-                        '&act=wordpress';
+			  '&api=serialize'.
+              '&act=wordpress';
 
-$post = array('insid' => '26_31793',
+$post = array('insid' => '26_14545',
               'admin_username' => 'admin',
               'admin_password' => 'new_password',
               'save_admin_info' => '1'
-);
+			);
 
 // Set the curl parameters.
 $ch = curl_init();
