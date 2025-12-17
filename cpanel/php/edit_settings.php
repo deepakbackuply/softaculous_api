1bbe660595b5a95ef695efdc94ccace793fab754
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 14:02:55 +0530

    Update edit_settings.php

--- cpanel/php/edit_settings.php
+++ cpanel/php/edit_settings.php
@@ -2,8 +2,8 @@
 
 // The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			 '&api=serialize'.
-			 '&act=settings';
+			  '&api=serialize'.
+			  '&act=settings';
 
 $post = array('editsettings' => 1,
 			  'language' => 'english',
