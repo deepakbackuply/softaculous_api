092cedd2424147ccfc8f8478729f33e28c98954a
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 13:53:17 +0530

    Update clone.php

--- cpanel/php/clone.php
+++ cpanel/php/clone.php
@@ -2,9 +2,9 @@
 
 // The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-			'&act=sclone'.
-			'&insid=26_12345';
+			  '&api=serialize'.
+			  '&act=sclone'.
+			  '&insid=26_12345';
 
 $post = array('softsubmit' => '1',
               'softdomain' => 'example.com', // Must be a valid Domain
