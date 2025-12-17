55eca46289f0fc59fae492cfc05c5656f6cbd480
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 14:53:50 +0530

    Update list_installed_themes.php

--- cpanel/php/list_installed_themes.php
+++ cpanel/php/list_installed_themes.php
@@ -2,13 +2,13 @@
 
 //The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-            '&act=wordpress';
+			  '&api=serialize'.
+              '&act=wordpress';
 
 $post = array('insid' => '26_31793',
               'type' => 'themes',
               'list' => '1'
-);
+			);
 
 // Set the curl parameters.
 $ch = curl_init();
