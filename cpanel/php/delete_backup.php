2ac96e6c0d7c5f16cc6a1baab5643919f9cb9cec
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 13:54:25 +0530

    Update delete_backup.php

--- cpanel/php/delete_backup.php
+++ cpanel/php/delete_backup.php
@@ -2,10 +2,9 @@
 
 // The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-			'&api=serialize'.
-			'&act=backups'.
-			'&remove=backup_time_insid.tar.gz';
-
+				'&api=serialize'.
+				'&act=backups'.
+				'&remove=backup_time_insid.tar.gz';
 
 // Set the curl parameters.
 $ch = curl_init();
