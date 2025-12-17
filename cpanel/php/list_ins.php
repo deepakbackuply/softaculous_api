3b999bffb10999c9b878db63f2420186f287ed14
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 14:52:55 +0530

    Update list_ins.php

--- cpanel/php/list_ins.php
+++ cpanel/php/list_ins.php
@@ -2,8 +2,8 @@
 
 // The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
-       '&api=serialize'.
-       '&act=installations';
+                   '&api=serialize'.
+                   '&act=installations';
 
 
 // Set the curl parameters.
