d9eaf00b901c12cdc071801e908bb18e38ef4ed6
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 15:12:31 +0530

    Update wp_remove_plugin.php

--- cpanel/php/wp_remove_plugin.php
+++ cpanel/php/wp_remove_plugin.php
@@ -3,13 +3,13 @@
 //The URL
 $url = 'https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?'.
 			'&api=serialize'.
-                        '&act=wordpress';
+            '&act=wordpress';
 
 $post = array('insid' => '26_31793',
               'type' => 'plugins',
               'slug' => 'akismet/akismet.php',
               'delete' => '1'
-);
+			);
 
 // Set the curl parameters.
 $ch = curl_init();
