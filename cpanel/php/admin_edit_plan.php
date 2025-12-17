f16e3c77c4fe14bffdd875f354a963ea5eb0257c
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-17 12:01:14 +0530

    Create admin_edit_plan.php

---
+++ cpanel/php/admin_edit_plan.php
@@ -0,0 +1,53 @@
+<?php 
+
+// URL
+$url = 'http://admin.controlpanel.com:PORT/url/to/softaculous/index.php?'.
+			'&api=serialize'.
+			'&act=editplans'.
+			'&plan=plan1';
+
+
+$post = array('saveplan' => '1',
+	'planname' => 'plan1',
+	'resellers_abc' => '1',
+	'users_xyz' => '1',
+        'cpplan_CPPlanName' => '1',
+	'scripts_543' => '1', // Add Drupal
+	'scripts_72' => '1' // Add PrestaShop
+);
+
+// Set the curl parameters.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// Turn off the server and peer verification (TrustManager Concept).
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+if(!empty($post)){
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+}
+ 
+// Get response from the server.
+$resp = curl_exec($ch);
+ 
+// The response will hold a string as per the API response method. In this case its PHP Serialize
+$res = unserialize($resp);
+ 
+// Done ?
+if(!empty($res['done'])){
+
+	print_r($res);
+
+// Error
+}else{
+
+	echo 'Some error occured';
+	print_r($res['error']);
+
+}
+
+?>
