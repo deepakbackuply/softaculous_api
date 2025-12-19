81f2ff06da48461c20a575594e277aa03b218291
deepakbackuply <133220892+deepakbackuply@users.noreply.github.com>
2025-12-19 11:10:50 +0530

    Delete deactivate_plugin.sh

--- cpanel/curl/deactivate_plugin.sh
+++
@@ -1,1 +0,0 @@
-curl -d "insid=26_31793" -d "type=plugins" -d "slug=akismet/akismet.php" -d "deactivate=1" "https://user:password@domain.com:2083/frontend/jupiter/softaculous/index.live.php?act=wordpress&api=json"
